d6751f523bfe0b84c547f8b97f0f9a179a58d475
Rolem Galvez <rolemgalvez@gmail.com>
2022-11-21 11:18:57 +0800

    Add project files.

---
+++ AverageGrade/Program.cs
@@ -0,0 +1,44 @@
+﻿//Welcome message
+Console.WriteLine("Average Grade");
+Console.WriteLine("-------------");
+Console.WriteLine();
+
+//Get user input
+Console.Write("Enter grade in 1st Quarter: ");
+double.TryParse(Console.ReadLine(), out double firstGrade);
+
+Console.Write("Enter grade in 2nd Quarter: ");
+double.TryParse(Console.ReadLine(), out double secondGrade);
+
+Console.Write("Enter grade in 3rd Quarter: ");
+double.TryParse(Console.ReadLine(), out double thirdGrade);
+
+//Process input
+double averageGrade = (firstGrade + secondGrade + thirdGrade) / 3;
+bool isGoodGrade = averageGrade >= 70 && averageGrade <= 79.99;
+bool isBetterGrade = averageGrade >= 80 && averageGrade <= 89.99;
+bool isBestGrade = averageGrade >= 90 && averageGrade <= 100;
+bool isUnbelievableGrade = averageGrade > 100;
+bool isOneTimeNiceGrade = firstGrade >= 70 || secondGrade >= 70 || thirdGrade >= 70;
+
+//Show results
+if (isGoodGrade)
+{
+    Console.WriteLine($"Your average grade of {averageGrade} reflects your effort.");
+}
+else if (isBetterGrade)
+{
+    Console.WriteLine($"Great job to have the average grade of {averageGrade}!");
+}
+else if (isBestGrade)
+{
+    Console.WriteLine($"Fantastic average grade of {averageGrade}!");
+}
+else if (isUnbelievableGrade)
+{
+    Console.WriteLine($"Average grade of {averageGrade}. That's unbelievable!");
+}
+else if (isOneTimeNiceGrade)
+{
+    Console.WriteLine($"You got one nice grade! But average grade of {averageGrade}.");
+}
---
+++ CanYouPass/Program.cs
@@ -0,0 +1,38 @@
+﻿//Welcome message
+Console.WriteLine("Can You Pass");
+Console.WriteLine("------------");
+Console.WriteLine();
+
+//Get user input
+Console.Write("What is your name: ");
+string? name = Console.ReadLine();
+
+Console.Write("Enter your General Weighted Average: ");
+bool isGwa = decimal.TryParse(Console.ReadLine(), out decimal gwa);
+
+//Evaluate input
+bool isJuan = name.ToLower() == "juan";
+bool isPedro = name.ToLower() == "pedro";
+bool gradePass = gwa >= 70;
+
+if (isJuan && gradePass)
+{
+    Console.WriteLine("Hello Juan Dela Cruz. You pass!");
+}
+else if (isPedro && gradePass)
+{
+    Console.WriteLine($"Nice {name}! You pass with your grade {gwa}.");
+}
+else if (isJuan || gradePass)
+{
+    Console.WriteLine($"Hi {name}. It is {gradePass} that you pass.");
+}
+else
+{
+    Console.WriteLine("You have room for improvements.");
+}
+
+if(gwa > 100)
+{
+    Console.WriteLine("Gotta say, you pass the limit, Saitama.");
+}
