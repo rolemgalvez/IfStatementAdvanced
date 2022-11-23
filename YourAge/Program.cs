500eeb62c9ae395bb64989be262bd4c1bb144a08
Rolem Galvez <rolemgalvez@gmail.com>
2022-11-23 09:44:50 +0800

    feature: done project YourAge

--- YourAge/Program.cs
+++ YourAge/Program.cs
@@ -16,3 +16,24 @@ bool isChild = age >= 0 && age <= 14;
 bool isYouth = age >= 15 && age <= 24;
 bool isAdult = age >= 25 && age <= 64;
 bool isSeniors = age >= 65;
+
+//Show values
+if (isChild || isYouth)
+{
+    Console.WriteLine($"You have way to go {name}!");
+}
+else if (isAdult)
+{
+    Console.WriteLine($"Take your time {name}.");
+}
+else if (isSeniors)
+{
+    Console.WriteLine($"Appreciate your contributions {name}.");
+    Console.WriteLine("Invest to next generation.");
+}
+else
+{
+    Console.WriteLine($"Well {name}, age doesn't matter anyways.");
+}
+
+Console.ReadLine();
